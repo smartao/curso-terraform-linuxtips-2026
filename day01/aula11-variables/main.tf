7cefc2ff52e9f756b29d6ff42a215b2818a3f9c9
smartao <smartao@gmail.com>
2026-01-25 16:52:02 -0300

    finalizado aula11 day01

---
+++ day01/aula11-variables/main.tf
@@ -0,0 +1,31 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = var.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+    Env  = "develop"
+  }
+}
+
+# Fontes
+# https://developer.hashicorp.com/terraform/language/values/variables
+# https://developer.hashicorp.com/terraform/language/validate
+# https://developer.hashicorp.com/terraform/language/manage-sensitive-data
