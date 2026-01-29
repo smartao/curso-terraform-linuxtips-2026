34ab3fa6fcfe40c41c269fb27ad9abb79e382cc8
smartao <smartao@gmail.com>
2026-01-28 21:50:20 -0300

    finalizado aula01 day03

---
+++ day03/aula01-modules/main.tf
@@ -0,0 +1,8 @@
+module "projetoa" {
+  source = "./instancias"
+  nome   = "projetoA"
+  environment = "Production"
+}
+
+# Fonte
+# https://developer.hashicorp.com/terraform/language/modules
---
+++ day03/aula01-modules/provider.tf
@@ -0,0 +1,26 @@
+ terraform {
+
+  backend "s3" {
+    bucket = "sergei-curso-linuxtips-2026"
+    key    = "day03-aula01-modules"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "west"
+  region = "us-west-2"
+}
